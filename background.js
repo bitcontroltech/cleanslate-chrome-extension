e34edb967ac181e5529a40906bc368ee4ec3ddff
Kristina Lim <kristinalim.ph@gmail.com>
2021-10-22 20:55:44 +0800

    Add deletion of cookies for domain name of active tab

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "OpenPopup") {
     openPopupPerform(message, sender, sendResponse)
+  } else if (message.action === "DeleteCookies") {
+    deleteCookiesPerform(message, sender, sendResponse)
   }
 
   return true
@@ -14,5 +16,16 @@ let openPopupPerform = async (message, sender, sendResponse) => {
       let tabUrl = tabs[0].url
       sendResponse({ url: tabUrl })
     }
+  })
+}
+
+let deleteCookiesPerform = async (message, sender, sendResponse) => {
+  chrome.cookies.getAll({ domain: message.domain }, (cookies) => {
+    for (var cookie of cookies) {
+      chrome.cookies.remove({ name: cookie.name, url: `http://${cookie.domain}${cookie.path}`})
+      chrome.cookies.remove({ name: cookie.name, url: `https://${cookie.domain}${cookie.path}`})
+    }
+
+    sendResponse({ success: true })
   })
 }
--- popup.js
+++ popup.js
@@ -1,7 +1,25 @@
+let data = {}
+
 chrome.runtime.sendMessage({ action: "OpenPopup" }, (response) => {
   let url = new URL(response.url)
   let domain = url.hostname
 
   let domainLabel = window.document.getElementById("detect__domain")
+  data.domain = domain
   domainLabel.innerHTML = domain
+})
+
+let buttonDeleteCookies = document.querySelector("#button__delete_cookies")
+
+buttonDeleteCookies.addEventListener("click", event => {
+  let button = event.target
+  let originalText = button.text
+
+  button.classList.add("processing")
+  button.innerHTML = "Processing..."
+
+  chrome.runtime.sendMessage({ action: "DeleteCookies", domain:  data.domain }, (response) => {
+    button.innerHTML = originalText
+    button.classList.remove("processing")
+  })
 })
